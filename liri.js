0873a833f7e33c3848e0d1e81584823b70840011
Tassa Tenhouse <kmtenhouse@gmail.com>
2018-10-17 01:46:33 -0700

    Added the ability to review menu results multiple times

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//CODE THAT RUNS WHEN WE RUN THIS PROGRAM:
+//CODE THAT RUNS WHEN WE FIRST RUN THIS PROGRAM:
 //First, check what the user has input as a command...
 var userCommand = process.argv[2]; 
 
@@ -43,25 +43,34 @@ function liri(command, arguments) {
 }
 
 function lookUpBands(arguments) {
-    console.log("Searching for bands...");
+
     //first, join together the band name...
     var bandName = arguments.join(" ");
+
+    //log the command itself to our logfile
+    logFile("concert-this " + bandName);
+
     //NOTE: we don't have a default band, so we'll just error and exit if no band name was provided
     if(bandName==="") {
         console.log("Please provide a bandname in order to search for concerts!");
+        logFile("Error: no band name was provided.");
         return;
     }
 
+    console.log("Searching for appearances by " + bandName +"...");
     //otherwise, we attempt to make the request!
     var request = require('request');
     request("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp", function (error, response, data) {
-    if(error) {
-       return console.log('Sorry, something went wrong:', error); // Print the error if one occurred
+        if(error) {
+            return console.log('Sorry, something went wrong:', error); // Print the error if one occurred
         }
-        if(response.statusCode===200) { //if we got a positive response, go through each of the events and grab exactly what we want   
-            var moment = require('moment'); 
+
+        //NOTE: there appears to be a problem in the object that bandsintown sends back when you input garbage data (a band that doesn't exist)
+        var eventList = JSON.parse(data); //note: we expect to receive back an array of objects
+        if(eventList.length>0) { 
+            //if we did receive results for this band, go through each of the events and grab exactly what we want
+            var moment = require('moment');   
             var allEvents = [];
-            var eventList = JSON.parse(data);
 
             eventList.forEach(currentEvent => {
                 //put together a reasonable location string based on what we have:
@@ -71,7 +80,7 @@ function lookUpBands(arguments) {
 
                 //now put together a simplified object to store all our info!
                 var newConcert = {
-                    band: bandName,
+                    band: currentEvent.lineup.join(", "),
                     venue: currentEvent.venue.name,
                     location: location,
                     date: humanreadableDate
@@ -79,17 +88,19 @@ function lookUpBands(arguments) {
                 allEvents.push(newConcert); 
             });
 
-            //log all results to our log file
-            logFile("concert-this", bandName, allEvents);
-
             //lastly, show the results to the user
             var customText = [
-            {property: "venue", description: "Live at"},  
+            {property: "venue", description: "Live at"}, 
+            {property: "band", description: "Full Lineup:"}, 
             {property: "date", description: "Appearing on"}, 
             {property: "location", description: "In"}, 
             ];
 
             displayResults(allEvents, {summarizeBy: ["date", "location"], separator: "", descriptors: customText});
+        }
+        else {
+            console.log("Sorry, " + bandName + " must not be on tour. I couldn't find any concerts for them.");
+            logFile("Error: no concerts are currently found for the band " + bandName);
         }
     });
 
@@ -101,6 +112,8 @@ function lookUpSong(arguments) {
     //First, see if the user provided anything - if not, you'll use a default
     var songName; 
     (arguments.length>0? songName = arguments.join(" ") : songName = "The Sign by Ace of Base");
+    console.log("Searching for " + songName + "...");
+    //log what we attempted to search for:
     //next, initialize what we need to make a call to spotify api
     var dotenv= require("dotenv").config(); //configure the .env to process environment variables
     var keys = require('./keys.js'); //export our keys using the wrapper keys.js
@@ -115,10 +128,15 @@ function lookUpSong(arguments) {
         if (err) {
             return console.log('Sorry, an error occurred: ' + err);
         }
-        console.log("Searching for " + songName + "...");
-        //add the results to our logfile
-        // logFile("#Command: spotify-this" + " " + songName, JSON.stringify(data, null,2));    
+
+        logFile("spotify-this-song " + songName);
         var allSongs = data.tracks.items;
+        //first, check if we received any song info - if not, end the search 
+        if(allSongs.length===0) {
+            console.log("Sorry, I didn't find any songs by that name.");
+            logFile("Error: no song found by the name " + songName);
+            return;
+        }
         //now we create an array of just the data we need (so that the user can interact with our UI)
         var truncatedSongs =[];
         for(let j=0; j<allSongs.length; j++) {
@@ -143,8 +161,6 @@ function lookUpSong(arguments) {
             truncatedSongs.push(currentItem);
         }
 
-        //log the results of this search in our log file
-        logFile("spotify-this-song", songName, truncatedSongs);
 
         //lastly, display the results to the user
         var customText = [
@@ -162,6 +178,11 @@ function lookUpMovie(arguments) {
     //first, determine if we have a movie or not...if not, our default will be 'Mr. Nobody'
     var movieName;
     (arguments.length>0 ? movieName = arguments.join(" ") : movieName = "Mr. Nobody");
+    
+    //log our search info to the logfile
+    var moment = require('moment');
+    logFile("movie-this " + movieName);
+
     //begin a request
     var request = require('request');
     request('http://www.omdbapi.com/?apikey=trilogy&t='+movieName, function (error, response, result) {
@@ -170,9 +191,10 @@ function lookUpMovie(arguments) {
             }
         
           //otherwise if we received a response, attempt to grab the results we want!
-            currentMovie = JSON.parse(result);
-            if(response.statusCode===200) {
+            currentMovie = JSON.parse(result); //parse the results into a JSON object
+            if(currentMovie.Response==="True") { //if the search response was true, there is a movie for us to look at!
                 console.log("Searching for the movie " + movieName + "...");
+               
                 var myMovie = [{
                     title: currentMovie.Title,
                     year: currentMovie.Year,
@@ -185,64 +207,83 @@ function lookUpMovie(arguments) {
                 }];
 
              //NOTE: we have to do a little extra work to find rotten tomatoes rating
-            for(let i=0; currentMovie.Ratings; i++) {
-                if(currentMovie.Ratings[i].Source==="Rotten Tomatoes") {
-                    myMovie.rottentomatoes = currentMovie.Ratings[i].Value;
-                    break;
+                for(let i=0; currentMovie.Ratings; i++) {
+                    if(currentMovie.Ratings[i].Source==="Rotten Tomatoes") {
+                        myMovie.rottentomatoes = currentMovie.Ratings[i].Value;
+                        break;
+                    }
                 }
+                //finally our result object is complete!
+               
+                //Finally, show all results to the user...
+                var customText = [
+                    {property: "title", description: "Movie Title:"}, 
+                    {property: "year", description: "Year Released:"}, 
+                    {property: "imdb", description: "IMDB Rating:"},
+                    {property: "rottentomatoes", description: "Rotten Tomatoes Score:"},
+                    {property: "producedIn", description: "Production Occurred In:"},
+                    {property: "language", description: "Languages:"},
+                    {property: "plot", description: "Synopsis:"},
+                    {property: "actors", description: "Starring:"}
+                ];
+                 displayResults(myMovie, {summarizeBy: ["title", "year"], separator: "(", descriptors: customText});
             }
-            //finally our result object is complete!
-            //log our search info to the logfile
-            logFile("movie-this", movieName, myMovie);
-
-            //Finally, show all results to the user...
-            var customText = [
-                {property: "title", description: "Movie Title:"}, 
-                {property: "year", description: "Year Released:"}, 
-                {property: "imdb", description: "IMDB Rating:"},
-                {property: "rottentomatoes", description: "Rotten Tomatoes Score:"},
-                {property: "producedIn", description: "Production Occurred In:"},
-                {property: "language", description: "Languages:"},
-                {property: "plot", description: "Synopsis:"},
-                {property: "actors", description: "Starring:"}
-            ];
-            displayResults(myMovie, {summarizeBy: ["title", "year"], separator: "(", descriptors: customText});
-         }
+            else { //otherwise, the response was false - the movie database didn't have anything by that name
+                console.log("Sorry, I couldn't find any movies by that name.");
+                logFile("Error: no movie found by the name " + movieName);
+            }
     });   
 }
 
-//attempts to parse instructions that are found within a file - assumes they are comma separated
+//attempts to parse liri instructions that are found within a file - assumes they are comma separated
+//in the form <command>,<arguments>
 function lookUpInstructions(arguments) {
-    //first, determine if the user tried to provide a filename...if not, we default to 'random.txt'
+    //first, determine if the user tried to provide a filename in their arguments
+    //if not, we will default to 'random.txt'
     var filename; 
     (arguments.length>0 ? filename = arguments.join("") : filename = "random.txt");
+    console.log("Reading liri commands from " + filename + "...");
 
-    //attempt to open the file and read in its contents
     var fs = require('fs');
+    //attempt to open the file and read in its contents
     fs.readFile(filename, 'utf8', function(err, data) {
         if(err) {
-            return console.log("Sorry, there was a problem reading from the file (" + filename + "): " + err);
+            return console.log("Sorry, there was a problem reading from the file (" + filename + "):\n" + err);
         }
+        logFile("do-what-it-says " + filename);
         //first, parse the results into an array (assuming that the original file is comma separated)
-        var fileResults = data.split(",");
-        //the first thing in the results array should be the command, the remainder is the arguments
-        var fileCommand = fileResults.shift();
-        //now we call liri based on the command + the array of any remaining arguments
-        liri(fileCommand, fileResults);       
+        var fileArguments = data.split(",");
+
+        //pop off the first thing we encounter in the file results, as we assume it's the command
+        var fileCommand = fileArguments.shift();
+
+        if(!isLiriCommand(fileCommand)) {
+            console.log("I'm sorry, your file needs to start with a valid liri command, then a comma before any arguments.");
+            logFile("Error: file contains an invalid command sequence.");
+            return;
+        }
+
+        //and call liri based on the command + the array of any remaining arguments
+        liri(fileCommand, fileArguments);
     });
+}
+
+//determines if a string is a valid liri command
+function isLiriCommand(word) {
+    return ((word==="spotify-this-song")||(word==="movie-this")||(word==="do-what-it-says")||(word==="concert-this")||(word==="help"));
 }
 
 //logs commands/arguments/searches to a log file
 //assumes that results will be objects that need to be stringified
-function logFile(command, searchTerm, result) {
+function logFile(whatToLog) {
     var fs = require('fs');
-    //assemble our (commented out) command string
-    var logCommand = "//" + command + " " + searchTerm;
+    var moment = require('moment');
     //stringify any objects
-    if(typeof(result)==="object") {
-        var logData = JSON.stringify(result, null, 2);
+    if(typeof(whatToLog)==="object") {
+        whatToLog = JSON.stringify(whatToLog, null, 2);
     }
-    fs.appendFile('log.txt', logCommand+"\n"+logData+"\n", 'utf8', function(err) {
+
+    fs.appendFile('log.txt',"[" + moment().format("MM/DD/YYYY h:mm a") +"] " + whatToLog+"\n", 'utf8', function(err) {
         if(err) {
             return console.log("Sorry, there was an error writing to the log file: " + err);
         }
@@ -253,9 +294,9 @@ function logFile(command, searchTerm, result) {
 //this function allows the user to look through the results of their previous search in a user-friendly way 
 //assumes that 'resultsArr' is an array of objects, and 'messages' is a message object that tells us what to display
 function displayResults(resultsArr, messages) {
-
-    //first, create the list of summary string(s)
+    //first, create the list of summary string(s) that we'll be displaying to the user in a custom menu
     var allSummarizedChoices = [];
+    //for each object in the array of results that we provided...
     resultsArr.forEach(function(currentObj, index) {
         var summarizedString = "";
         for(let i=0; i<messages.summarizeBy.length; i++) {
@@ -265,41 +306,71 @@ function displayResults(resultsArr, messages) {
                     summarizedString+=" (";
                 }
                 else {
-                    summarizedString+=" " + messages.separator+" "; //otherwise it's just a separator buffered by spaces
+                    summarizedString+=" " + messages.separator+" "; //otherwise it's just the separator buffered by spaces
                 }
             }
             summarizedString+=currentObj[propertyName]; //and add it to the string
             //check for closing parens also
-            if(i>0) {
-                if(messages.separator==="(") {
-                    summarizedString+=")"; //if we are doing parens, add a closing parens
-                }
+            if(i>0 && messages.separator==="(") {
+                summarizedString+=")"; //if we are doing parens, add a closing parens
             }
         }
         //now add an OBJECT to our array of summarized choices -- the name will be our summarized string, and the value we actually grab is the index
         allSummarizedChoices.push({name: summarizedString, value: index}); 
     });
+    
+    //finally, add a 'choice' that is our signal to quit: a choice with a value of -1 
+    allSummarizedChoices.push({name: 'Quit', value: -1});
 
-    //next, fire up inquirer with our customized list of choices
+    //now, fire up inquirer with our customized list of choices -- using recursion to manage this
     var inquirer = require('inquirer');
     var questions = [ {
         type: 'list',
         name: 'userChoice',
-        message: 'I found the following results:',
+        message: "Here's everything I found:",
         choices: allSummarizedChoices
       }];
 
+    //NOTE: we define and use this recursion to keep showing results untilt he user tells us to stop :)
+    function displayChoiceMenu() {
       inquirer.prompt(questions).then(answers => {
-        //find the index of the choice we selected:
+        //find the index of the choice we selected
+        //if it's -1, we're going to stop!
         var index = answers.userChoice;
-        //grab the specific object we want to display
-        var selectedItem = resultsArr[index]; 
-        //now, go through our messages and display the info using our custom text
-        console.group();
-        messages.descriptors.forEach(function(desc) {
-            console.log(desc.description + " " + selectedItem[desc.property]);
-        });
-        console.groupEnd();
-      });
+        if(index>=0) {
+            //put a separator in our logfile
+            //grab the specific object we want to display
+            var selectedItem = resultsArr[index]; 
+            //now, go through our messages and display the info using our custom text
+            var logItems = "";
+            console.group();
+            messages.descriptors.forEach(function(desc) {
+            //NOTE: here we check that the object has the property we're looking for before trying to output it!
+            if(selectedItem.hasOwnProperty(desc.property)) {
+                //display the customized description for each property...
+                console.log(desc.description + " " + selectedItem[desc.property]);
+                //..and log the same to our logFile
+                logItems+=desc.description + " " + selectedItem[desc.property] + "\n";
+            }
+             });
+            console.groupEnd();
 
+            //now recurse to display our menu again!
+            //NOTE: we will ALSO stop recursing if there was only one result -- no reason to go back to the menu
+            logFile("\n" + logItems+"==============");
+            if(resultsArr.length>1) {
+                displayChoiceMenu();
+            }
+            else {
+                console.log("That's all I have for this topic! Goodbye!");
+            }
+        }
+        else { //otherwise, if we selected our 'quit' option, we are quitting liri
+            console.log("Goodbye!");
+        }
+      });
+    }
+    //Call the menu display at least once to get it started
+    displayChoiceMenu();
 }
+
